cb77aaf4052764e66976535d93ffd6c83c7e24fd
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 13:18:22 +0100

    exo 11

--- 11-arrow-functions-basics/1-rewrite-arrow/README.md
+++ 11-arrow-functions-basics/1-rewrite-arrow/README.md
@@ -4,11 +4,14 @@
 Remplacez les expressions de fonction par des fonctions fléchées dans le code ci-dessous :
 
 ```js run
-function ask(question, yes, no) {
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+ask = () =>{
   if (confirm(question)) yes();
   else no();
-}
-
+};
 ask(
   "Do you agree?",
   function() { alert("You agreed."); },
--- 11-arrow-functions-basics/1-rewrite-arrow/script.js
+++ 11-arrow-functions-basics/1-rewrite-arrow/script.js
@@ -2,3 +2,18 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+ask = (question,yes,no) =>{
+      if (confirm(question)) yes();
+      else no();
+    };
+ask(   
+      "Do you agree?",
+      () => alert("You agrid") ,
+      () => alert("you canceled")
+      // function() { alert("You agreed."); },
+      // function() { alert("You canceled the execution."); }
+    );
