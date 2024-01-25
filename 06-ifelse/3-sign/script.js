30f516fa94af405161cf7527a62603cf47a8ad48
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 10:01:01 +0100

    exo 5-3

--- 06-ifelse/3-sign/script.js
+++ 06-ifelse/3-sign/script.js
@@ -2,3 +2,12 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let userPrompt = prompt("Entrez un chiffre", 0);
+
+if (userPrompt > 0) {
+      alert(1);
+} else if (userPrompt < 0) {
+      alert(-1);
+} else {
+      alert(0);
+}
