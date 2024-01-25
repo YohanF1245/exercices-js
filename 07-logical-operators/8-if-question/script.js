ec41bb862cfc81fba33d75e5564a07dc591b94a6
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 11:09:21 +0100

    exo 7-9

--- 07-logical-operators/8-if-question/script.js
+++ 07-logical-operators/8-if-question/script.js
@@ -2,3 +2,6 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+if (-1 || 0) alert( 'first' ); //<--
+if (-1 && 0) alert( 'second' );
+if (null || -1 && 1) alert( 'third' );//<--
--- 07-logical-operators/9-check-login/script.js
+++ 07-logical-operators/9-check-login/script.js
@@ -2,3 +2,23 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let userName = prompt("entez user name", "name");
+
+if (userName === "Admin") {
+      continuePass();
+} else if (userName === "" || userName === null) {
+      alert("Cancelled");
+} else {
+      alert("idk");
+}
+
+function continuePass() {
+      let userPass = prompt("entrez pass", "pass")
+      if (userPass === "TheMaster") {
+            alert("welcome");
+      } else if(userPass === "" || userName === null ){
+            alert("cancelled");
+      } else {
+            alert("cancelled");
+      }
+}
