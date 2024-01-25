1170d49f957dcc98e80abc14420b52c5dbd962ed
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 08:54:22 +0100

    exo 3

--- 03-alert-prompt-confirm/1-simple-page/script.js
+++ 03-alert-prompt-confirm/1-simple-page/script.js
@@ -2,3 +2,6 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+function showName() {
+      document.getElementById("nameGoesHere").innerText = document.getElementById("name").value;
+}
