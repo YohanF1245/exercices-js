505c4fa36db3ef560fc0cf41679d1881bc9b815c
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 11:20:58 +0100

    exo 8

--- 08-type-conversions/script.js
+++ 08-type-conversions/script.js
@@ -2,3 +2,15 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let value = true;
+alert(typeof value);
+alert(value.toString());
+let str = "123";
+alert(typeof str);
+alert(parseInt(str));
+let nb = 1;
+alert(nb);
+nb = (nb === 1);
+alert(nb);
+nb = (nb === 0);
+alert(nb);
