fdac9b67646e17a9600945624e2d0b23e6b2a4bf
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 12:08:45 +0100

    exo 10-4

--- 10-function-basics/4-pow/script.js
+++ 10-function-basics/4-pow/script.js
@@ -2,3 +2,15 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let x = prompt("entrez la valeur de x", "valeur");
+let y = prompt("entrez la valeur de y","valeur")
+alert(powpow(x, y));
+function powpow(a, b) {
+      //      3 ^ 3
+      var tempResult = a;
+      for (var i = 1; i < b; i++){
+            tempResult = tempResult * a; 
+      }
+      return (tempResult);
+}
+//ou alors y'a math.pow
