a080077f3bca9b1ae7d5826ab3e13deb23f37e24
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 10:50:23 +0100

    exo 7-6

--- 07-logical-operators/6-check-if-in-range/script.js
+++ 07-logical-operators/6-check-if-in-range/script.js
@@ -2,3 +2,7 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+var age = prompt("entrez age", "age");
+if (age > 13 && age < 91) {
+      alert(age)
+}
