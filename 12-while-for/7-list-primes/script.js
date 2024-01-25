2415d70b3248c2a4d75b6af520be1dcc39e3ffc1
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 14:32:30 +0100

    exo 12-7

--- 12-while-for/7-list-primes/script.js
+++ 12-while-for/7-list-primes/script.js
@@ -2,3 +2,23 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let primeTab = [];
+let isPrime = true;
+function calcPrime() {
+      primeTab = [];
+      console.log("function loaded")
+      let userPrompt = document.getElementById("userInput").value;
+      for (i = 1; i <= userPrompt; i++){
+            isPrime = true;
+            for (j = 2; j <= i; j++){
+                  if (i % j === 0 && i!=j) {
+                        isPrime = false;
+                  }
+            }
+            if (isPrime) {
+                  primeTab.push(i);
+            }
+      }
+      document.getElementById("primeList").innerText = "liste des nombres premiers : " + primeTab;
+      console.log(primeTab);
+}
