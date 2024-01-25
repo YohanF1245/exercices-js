70cc753ceab5b1b50093e6b3b0f3c7a76ea609dd
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 08:32:53 +0100

    exo 1-3

--- 01-variables/3-uppercast-constant/script.js
+++ 01-variables/3-uppercast-constant/script.js
@@ -2,3 +2,13 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+function showAnswer(){
+      const answerEx = document.createElement("p");
+      answerEx.innerText = "Il est possible d'utiliser des masjuscules pour déclarer des variables ou des constantes en JS. Par convention, il est preferable de declarer des constantes en majuscule pour des soucis de lisibilité du code";
+      answerEx.style.backgroundColor = "white";
+      answerEx.style.width = "50%";
+      answerEx.style.margin = "auto";
+      answerEx.style.fontSize = "30px";
+      answerEx.style.textAlign = "center";
+      document.body.appendChild(answerEx);
+}
