50e24c6ca33ca79a75987809b9dae2731ca91beb
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 10:56:20 +0100

    exo 7-7

--- 07-logical-operators/7-check-if-out-range/script.js
+++ 07-logical-operators/7-check-if-out-range/script.js
@@ -2,3 +2,12 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let age = prompt("entrez age", "age");
+
+if (age < 14 || age > 90) {
+      alert("test 1 reussi")
+}
+
+if (!(age > 13 && age < 91)){
+      alert("test 2 reussi")
+}
