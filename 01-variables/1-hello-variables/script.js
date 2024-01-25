e38c855c49c717841a2de4e13489395e59e6ddb4
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 08:18:58 +0100

    exo 1-1

--- 01-variables/1-hello-variables/script.js
+++ 01-variables/1-hello-variables/script.js
@@ -2,3 +2,7 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+var nameNotDeprecated, admin;
+nameNotDeprecated = "john";
+admin = nameNotDeprecated;
+alert(admin);
